46e2eeda440569492c54cb8394bf74090df66f22
Sachvk <serg22011985@gmail.com>
2024-10-28 15:13:20 +0300

    Update module_9_3.py

--- module_9_3.py
+++ module_9_3.py
@@ -1,7 +1,7 @@
 first = ['Strings', 'Student', 'Computers']
 second = ['Строка', 'Урбан', 'Компьютер']
 
-first_result = (len(i) - len(x) for i, x in zip(first, second))
+first_result = (len(i) - len(x) for i, x in zip(first, second) if len(i) != len(x))
 second_result = (len(first[i]) == len(second[i]) for i in range(len(first)))
 
 print(list(first_result))
